d1ff5854cd8d23e2b88dc64ec3bfc118300c4d99
jordondoug2019 <jdouglas2015@gmail.com>
2019-08-06 20:48:46 -0500

    practice variables and alerts

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 alert("Hello World");
 
-var schoolName= ("Code School ")
+var schoolName= (" Code School ")
 
 alert (" My school's name is" + schoolName)
+
+var namesOnly= (" Rick ")
+var namesONly2 = (" Shawn ")
+
+alert(namesOnly + " and " + namesONly2 + " attend " + schoolName)
